54b7ffb7feddd927c14570eec33784c3de40e12d
Mingyueyixi <1093286982@qq.com>
2022-03-26 22:39:53 +0800

    [mod] FragmentNavigation 改进

--- app/src/main/java/com/lu/code/magic/ui/FragmentNavigation.kt
+++ app/src/main/java/com/lu/code/magic/ui/FragmentNavigation.kt
@@ -3,46 +3,69 @@ package com.lu.code.magic.ui
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import java.util.*
+
 
 /**
  * @Author: Lu
  * Date: 2022/03/17
- * Description:
+ * Description: 简易的fragment导航工具类
  */
-class FragmentNavigation(
-    private var activity: FragmentActivity,
-    private var fragmentContainer: ViewGroup
-) {
-
+class FragmentNavigation : DefaultLifecycleObserver {
+    private lateinit var fragmentContainer: ViewGroup
+    private lateinit var supportFragmentManager: FragmentManager
     private var fragmentStack: Stack<Fragment> = Stack()
-    private val supportFragmentManager get() = activity.supportFragmentManager
     private var navigateIndex = -1
 
+    constructor(activity: FragmentActivity, fragmentContainer: ViewGroup) {
+        doInit(activity, fragmentContainer)
+    }
+
+    private fun doInit(activity: FragmentActivity, fragmentContainer: ViewGroup) {
+        this.supportFragmentManager = activity.supportFragmentManager
+        this.fragmentContainer = fragmentContainer
+    }
+
+    /**
+     * 导航到指定cls
+     */
+    fun navigate(fragClass: Class<out Fragment>) {
+        var tag = fragClass.toString()
+        var frag: Fragment? = supportFragmentManager.findFragmentByTag(tag)
+        if (frag == null) {
+            frag = fragClass.newInstance()
+        }
+        navigate(frag!!)
+    }
+
+    /**
+     * 导航到指定fragment
+     */
     fun navigate(fragment: Fragment) {
         var tag = fragment.javaClass.toString()
+        navigateInterval(fragment, tag)
+    }
 
-        var frag = supportFragmentManager.findFragmentByTag(tag)
+
+    private fun navigateInterval(fragment: Fragment, tag: String) {
         var transaction = supportFragmentManager.beginTransaction()
-
-        //主要导航片段
+        //主导航Fragment
         var primaryFragment = supportFragmentManager.primaryNavigationFragment
         if (primaryFragment != null) {
             transaction.hide(primaryFragment)
         }
-
-        if (frag == null || fragment != frag) {
-            frag = fragment
-            transaction.add(fragmentContainer.id, frag, tag)
-        } else {
-            transaction.show(frag)
+        if (!fragment.isAdded) {
+            transaction.add(fragmentContainer.id, fragment, tag)
         }
         //设置主导航fragment为当前要显示的导航fragment
         transaction.setPrimaryNavigationFragment(fragment);
-        transaction.setReorderingAllowed(true);
-        transaction.commitNow();
+        transaction.show(fragment).commitNow()
 
-        if (!fragmentStack.contains(frag) && navigateIndex < fragmentStack.size) {
+        //处理自己维护的fragment
+        if (!fragmentStack.contains(fragment) && navigateIndex < fragmentStack.size) {
 
             if (navigateIndex > -1 && navigateIndex != fragmentStack.size - 1) {
                 //当前显示的fragment不是最后一个，清空当前fragment之后的fragment记录
@@ -52,17 +75,19 @@ class FragmentNavigation(
                 }
 
             }
-            fragmentStack.add(frag)
-
+            fragmentStack.add(fragment)
         }
 
-        navigateIndex = fragmentStack.indexOf(frag)
+        navigateIndex = fragmentStack.indexOf(fragment)
     }
 
     fun findNavigateFragment(): Fragment? {
         return fragmentStack[navigateIndex]
     }
 
+    /**
+     * 退后
+     */
     fun navigateBack(): Boolean {
         var preIndex = navigateIndex - 1
         if (preIndex < 0 || preIndex >= fragmentStack.size) {
@@ -73,7 +98,10 @@ class FragmentNavigation(
         return true
     }
 
-    fun navigateFont(): Boolean {
+    /**
+     * 前进
+     */
+    fun navigateFront(): Boolean {
         var preIndex = navigateIndex + 1
         if (preIndex < 0 || preIndex >= fragmentStack.size) {
             return false
@@ -83,4 +111,8 @@ class FragmentNavigation(
         return true
     }
 
+    override fun onDestroy(owner: LifecycleOwner) {
+        super.onDestroy(owner)
+        fragmentStack.clear()
+    }
 }
